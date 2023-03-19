feeb75482272e1216fb68bf23a8c028b930dd84c
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-03-19 13:29:55 +0200

    Array task 4

--- array/Task2/Task2/Program.cs
+++ array/Task2/Task2/Program.cs
@@ -61,13 +61,59 @@ namespace Task2
                 array[i] = result[i];
             }
         }
-
+        /*2) Створіть метод int [] SubArray (int [] array, int index, int count).
+         * Метод повертає частину отриманого як аргумент масиву, починаючи 
+         * з позиції зазначеної в аргументі index, розмірністю, яка відповідає 
+         * значенню аргументу count. Якщо аргумент count містить значення більше, 
+         * ніж кількість елементів, які входять до частини вихідного масиву 
+         * (від зазначеного індексу index, до індексу останнього елемента), 
+         * то при формуванні нового масиву розмірністю в count, заповніть одиницями 
+         * ті елементи, які не були скопійовані з вихідного масиву
+         * */
         static int[] SubArray(int[] array, int index, int count)
         {
             int[] result = new int[count];
 
+            int j = 0;
+            for(int i = index; (j<count) && (i<array.Length); i++)
+            {
+                result[j] = array[i];
+                j++;
+            }
             
+            for(j=j; j < count; j++)
+            {
+                result[j] = 1;
+            }
+
             return result;
+        }
+
+        static void IncSize(ref int[] array)
+        {
+            Array.Resize(ref array, array.Length+1);  
+        }
+
+        /*
+         * Створіть метод, який приймає два аргументи, 
+         * перший аргумент типу int [ ] array, 
+         * другий аргумент типу int value. 
+         * У тілі методу реалізуйте можливість додавання другого 
+         * аргументу методу до масиву за індексом – 0, 
+         * при цьому довжина нового масиву повинна збільшитися
+         * на 1 елемент, а елементи одержуваного масиву як перший аргумент 
+         * мають скопіюватися в новий масив починаючи з індексу - 1. 
+         */
+        static void ShiftArray(ref int[] array, int value)
+        {
+            IncSize(ref array);
+
+            for(int i = array.Length-1; i>0; i--)
+            {
+                array[i] = array[i-1];
+            }
+
+            array[0] = value;
         }
 
         static void Main(string[] args)
@@ -77,7 +123,21 @@ namespace Task2
             if (int.TryParse(Console.ReadLine(), out n))
             {
                 int[] array = GenerateRandom(n);
+                Console.WriteLine("array");
+                PrintArray(array);
+                ShiftArray(ref array, 100);
+                Console.WriteLine("array with 100");
+                PrintArray(array);
 
+                /*
+                int[] copy = SubArray(array, 2, 3);
+                Console.WriteLine("array");
+                PrintArray(array);
+                Console.WriteLine("copy");
+                PrintArray(copy);
+                /*
+
+                /*
                 PrintArray(array);
 
                 Console.WriteLine($"max {MaxValue(array)}");
@@ -89,7 +149,8 @@ namespace Task2
                 ArrayReverse(array);
                 Console.WriteLine();
                 PrintArray(array);
+                */
             }
-        }
+}
     }
 }
