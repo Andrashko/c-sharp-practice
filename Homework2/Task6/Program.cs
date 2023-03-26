a41cebeaaba0205ba6404bf460b2bd691533408b
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-03-26 17:25:13 +0300

    вніс деякі зауваження в  коментарях

---
+++ Homework2/Task6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User ("Gressot", "Oleksandr", "Lutso", 35, DateTime.Now);
+
+            Console.WriteLine("\nДанi про користувача");
+            Console.WriteLine("\nЛогiн: " + user.Login);
+            Console.WriteLine("Iм'я: " + user.Name);
+            Console.WriteLine("Прiзвище: " + user.LastName);
+            Console.WriteLine("Вiк: " + user.Age);
+            Console.WriteLine("Дата заповнення: " + user.DateOfregister);
+
+            Console.ReadKey();
+        }
+    }
+}
