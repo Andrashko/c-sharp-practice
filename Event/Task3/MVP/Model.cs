3ee60d43ba6c31745e55abba93e694326fe3c970
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-04-21 12:08:07 +0300

    Task 3 with bug

---
+++ Event/Task3/MVP/Model.cs
@@ -0,0 +1,28 @@
+﻿// Model
+
+namespace MVP
+{
+    class Model
+    {
+        private double _value = 0;
+
+        public double Value { get { return _value; } }
+        public double Add(double value)
+        {
+            _value += value;
+            return _value;
+        }
+
+        public double Sub(double value)
+        {
+            _value += value;
+            return _value;
+        }
+
+        public double Mult(double value)
+        {
+            _value += value;
+            return _value;
+        }
+    }
+}
---
+++ Event/Task3/MVP/Presenter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Controls;
+
+// Presenter
+
+namespace MVP
+{
+    class Presenter
+    {
+        Model model = null;
+        MainWindow view = null;
+
+        public Presenter(MainWindow mainWindow)
+        {
+            this.model = new Model();
+            this.view = mainWindow;
+            this.view.AddClicked += addButton_Click;
+            this.view.SubClicked += subButton_Click;
+            this.view.MultClicked += multButton_Click;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var data = model.Add(view.FirstNumber + view.SecondNumber);
+            this.view.resultButton.Text = data.ToString();
+        }
+
+        private void subButton_Click(object sender, EventArgs e)
+        {
+            var data = model.Sub(view.FirstNumber - view.SecondNumber);
+            this.view.resultButton.Text = data.ToString();
+        }
+
+        private void multButton_Click(object sender, EventArgs e)
+        {
+            var data = model.Mult(view.FirstNumber * view.SecondNumber);
+            this.view.resultButton.Text = data.ToString();
+        }
+
+    }
+}
