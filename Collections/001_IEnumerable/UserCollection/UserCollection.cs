78dab18e73011901940bdd354037256d69a33c8f
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-05-03 17:06:27 +0300

    MyDate

---
+++ Collections/001_IEnumerable/UserCollection/UserCollection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+namespace InterIEnumerable
+{
+    // Клас UserCollection - колекція(набір) об'єктів класу Element.
+    // Для застосування foreach, необхідно, щоб клас реалізовував інтерфейс - IEnumerable.
+    public class UserCollection : IEnumerable, IEnumerator
+    {
+        public Element[] elementsArray = null;
+
+        public UserCollection()
+        {
+            elementsArray = new Element[4];
+            elementsArray[0] = new Element("A", 1, 10);
+            elementsArray[1] = new Element("B", 2, 20);
+            elementsArray[2] = new Element("C", 3, 30);
+            elementsArray[3] = new Element("D", 4, 40);
+        }
+
+        // Покажчик поточної позиції елемента в масиві.
+        int position = -1;
+
+        // ------------------------------------------------------------------------------------------------------------------
+        // Реалізація інтерфейсу IEnumerator.
+
+        // Пересунути внутрішній покажчик (position) однією позицію.
+        public bool MoveNext()
+        {
+            if (position < elementsArray.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+        }
+
+        // Встановити вказівник (position) перед початком набору.
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        // Отримати поточний елемент набору. 
+        public object Current
+        {
+            get { return elementsArray[position]; }
+        }
+
+        // -----------------------------------------------------------------------------------------------------------------
+        // Реалізація інтерфейсу - IEnumerable.
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+    }
+}
---
+++ DateOperator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DateOperator
+{
+    /*
+     * Створіть клас, який буде містити інформацію про дату 
+     * (день, місяць, рік).
+     * За допомогою механізму перевантаження 
+     * операторів, визначте операцію різниці двох дат 
+     * (результат у вигляді кількості днів між датами), 
+     * а також операцію збільшення дати на певну кількість днів.
+     */
+    class MyDate
+    {
+        private int _day;
+        public  int Day { get { return _day; } set
+            {
+                if ( value <= 0 || value > 31)
+                {
+                    throw new Exception("Wrong day");
+                }
+                _day = value;
+            }
+        }
+
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public MyDate(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
